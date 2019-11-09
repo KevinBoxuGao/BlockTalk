823dba51fdb6285c5df2e0d2f7042ea87b8213d6
Eric Xiao <erixrekt69@gmail.com>
2019-11-09 01:30:07 -0500

    Testing

--- fortmatic.js
+++ fortmatic.js
@@ -1,25 +1,33 @@
 // Initialize provider
-import Fortmatic from 'fortmatic';
-import Web3 from 'web3';
+
+//For Kevin: uncomment next two lines after npm installing
+//import Fortmatic from 'fortmatic';
+//import Web3 from 'web3';
 
 const fm = new Fortmatic('pk_test_9E6F1DC09BE9D836');
 window.web3 = new Web3(fm.getProvider());
 
 //Logs user in
-let handleLogin = async () => {
-    let accounts = await fm.user.login();
-    if (loggedIn()) {
-        let userData = await fm.user.getUser();
-    }
-}
-
-//Checks if user is logged-in
-let loggedIn = () => {
-    let isLoggedIn = await fm.user.isLoggedIn();
-    return isLoggedIn;
+let handleLogin = () => {
+    fm.user.login();
+    $('#section-login').hide();
+    $('#section-logout').show();
+    let userData = fm.user.getUser();
 }
 
 //Logout
 let handleLogout = () => {
     fm.user.logout();
+    $('#section-logout').hide();
+    $('#section-login').show();
+}
+
+window.onload = function() {
+    $('#section-logout').hide();
+    $('#btn-login').click(() => {
+        handleLogin();
+    });
+    $('#btn-logout').click(() => {
+        handleLogout();
+    });
 }
