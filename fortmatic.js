f94d0a924449f7e5f705d6c1762f2da4f3b168c3
Alexander Chudinov <alexander.chudinov@icloud.com>
2019-11-10 09:50:38 -0500

    push

---
+++ fortmatic.js
@@ -0,0 +1,170 @@
+const fm = new Fortmatic('pk_test_9E6F1DC09BE9D836');
+window.web3 = new Web3(fm.getProvider());
+
+var CURRENT_RECIPIENT =  ""
+var CURRENT_UID = ""
+
+//Logs user in
+let handleLogin = () => {
+    fm.user.login().then(() => {
+        console.log(fm.user.isLoggedIn());
+        $('.container').hide();
+        $('#logged-in').show();
+        let userData = fm.user.getUser().then(res => {
+            document.getElementById("btn-3-h3").innerText += "  "+truncate(res.userId,16,'...')
+            CURRENT_UID = res.userId
+        })
+    });
+}
+
+//Logout
+let handleLogout = () => {
+    fm.user.logout();
+    $('#logged-in').hide();
+    $('.container').show();
+    closeSideNav();
+}
+
+//Send message
+let renderMessage = (input = document.getElementById("messageInput").value) => {
+    var div = document.createElement("DIV");
+    //div.setAttribute("class", "messageBubble");
+    div.classList = ["messageBubble sender"]
+    //div.setAttribute("class", "sender");
+
+    var text = document.createTextNode(input);
+    div.appendChild(text);
+
+    var messages = document.getElementById('messages');
+    messages.appendChild(div)
+
+    document.getElementById('messageInput').value = "";
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    await response.clone().json()
+    return response.clone().json()
+}
+
+autoReloadChat = () => {
+    if(CURRENT_RECIPIENT!=""){
+        e = {target:{lastChild: CURRENT_RECIPIENT}}
+        loadChat(e)
+    }
+}
+
+//setInterval(autoReloadChat,3000)
+
+//Click a person's contact to open a chat
+function loadChat(e) {
+    to_addr = e.target.lastChild.innerText || e.target.lastChild
+    console.log(to_addr)
+    CURRENT_RECIPIENT = to_addr
+    document.getElementById("messages").innerHTML = ""
+
+    try {
+        postData('/loadMessages', {addr: to_addr}).then(data => {
+            console.log(data);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+
+    fetch('/json').then((res) => {
+        return res.json();
+    }).then(res1 => {
+        console.log(res1)
+        res1.data.reverse().forEach(message => {
+            if (message.text!="" && CURRENT_RECIPIENT==message.recipient) renderMessage(message.text)
+        })
+    })
+}
+
+sendMessage = () => {
+    input = document.getElementById("messageInput").value
+    try {
+        postData('/upload', {textToUpload:input,UID:CURRENT_UID,recipient:CURRENT_RECIPIENT}).then(data => {
+            console.log(data);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+//function for truncation in the middle of a string
+var truncate = function (fullStr, strLen, separator) {
+    if (fullStr.length <= strLen) return fullStr;
+
+    separator = separator || '...';
+
+    var sepLen = separator.length,
+        charsToShow = strLen - sepLen,
+        frontChars = Math.ceil(charsToShow/2),
+        backChars = Math.floor(charsToShow/2);
+
+    return fullStr.substr(0, frontChars) +
+           separator +
+           fullStr.substr(fullStr.length - backChars);
+}
+
+if(!document.cookie.contacts){
+    document.cookie = {
+        contacts : ""
+    }
+}
+
+//Adds a contact
+let addContact = () => {
+    if (document.getElementById('eth-field').value != '') {
+        var name = document.createElement('H3')
+        var raw = document.getElementById('eth-field').value
+        name.innerText = raw
+        if(document.cookie.contacts){
+            document.cookie = {
+                contacts: document.cookie+','+raw
+            }
+        } else {
+            document.cookie = {
+                contacts: raw
+            }
+        }
+        var div = document.createElement('DIV');
+        var img = document.createElement('IMG')
+        img.src = 'http://localhost:3000/ethereum'
+        img.setAttribute('class', 'account-icon')
+        div.setAttribute('class', 'btn-2');
+        div.appendChild(img)
+        div.appendChild(name);
+
+        var contacts = document.getElementById('contacts');
+        contacts.appendChild(div);
+
+        document.getElementById('eth-field').value = '';
+    }
+}
+
+//Allows for adding events to dynamic elements
+window.onload = function() {
+    $('#logged-in').hide(); //Hides in login page
+    //$('#chat').hide(); //Hides chat page
+    $('#enter-button').on('click', renderMessage); //Send message
+    $('#btn-login').on('click', () => { //Login
+        handleLogin();
+    });
+    $('#btn-logout').on('click', () => { //Logout
+        handleLogout();
+    });
+    $('#addContact button').on('click', addContact); //Adds a contact
+    $(document).on('click', '.btn-2', e => loadChat(e)); //Opens chat page
+}
---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const pug = require('pug')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+const fs = require('fs')
+
+const quit = (err) => {
+    console.log(err)
+    process.exit()
+}
+
+app.set('view engine', 'pug')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'web_resources')))
+
+app.get('/app', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, (err) => {
+    if(err){quit(err)}
+})
+
+const multer = require('multer')
+const upload = multer({ dest: '' })
+
+const ipfsAPI = require('ipfs-http-client')
+const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
+const isIPFS = require('is-ipfs')
+
+String.prototype.toBase64 = function () {
+    return new Buffer(this).toString('base64')
+}
+
+const ethers = require('ethers')
+
+const CONTRACT_ADDRESS = "0x7d2F3263989B1fB9C5A183625531CC915AA2c550";
+const CONTRACT_ABI = [{"constant":!1,"inputs":[{"name":"_to","type":"address"},{"name":"_text","type":"string"}],"name":"sendMessage","outputs":[],"payable":!1,"type":"function","stateMutability":"nonpayable"},{"constant":!1,"inputs":[{"name":"_key","type":"string"},{"name":"_type","type":"string"}],"name":"setPublicKey","outputs":[],"payable":!1,"type":"function","stateMutability":"nonpayable"},{"anonymous":!1,"inputs":[{"indexed":!0,"name":"_sender","type":"address"},{"indexed":!0,"name":"_receiver","type":"address"},{"indexed":!1,"name":"_time","type":"uint256"},{"indexed":!1,"name":"message","type":"string"}],"name":"Message","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!0,"name":"_sender","type":"address"},{"indexed":!1,"name":"_key","type":"string"},{"indexed":!1,"name":"_keytype","type":"string"}],"name":"PublicKeyUpdated","type":"event"},{"constant":!0,"inputs":[{"name":"_who","type":"address"}],"name":"getLastMessage","outputs":[{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"_who","type":"address"},{"name":"_index","type":"uint256"}],"name":"getMessageByIndex","outputs":[{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"_who","type":"address"}],"name":"getPublicKey","outputs":[{"name":"_key","type":"string"},{"name":"_key_type","type":"string"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"","type":"address"}],"name":"keys","outputs":[{"name":"key","type":"string"},{"name":"key_type","type":"string"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"","type":"address"}],"name":"last_msg_index","outputs":[{"name":"","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"_owner","type":"address"}],"name":"lastIndex","outputs":[{"name":"","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[],"name":"message_staling_period","outputs":[{"name":"","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"messages","outputs":[{"name":"from","type":"address"},{"name":"text","type":"string"},{"name":"time","type":"uint256"}],"payable":!1,"type":"function","stateMutability":"view"},{"constant":!0,"inputs":[{"name":"_who","type":"address"},{"name":"_index","type":"uint256"}],"name":"newMessage","outputs":[{"name":"","type":"bool"}],"payable":!1,"type":"function","stateMutability":"view"}]
+
+var jsondata = []
+
+app.post('/upload', upload.single(), (req,res) => {
+    window.web3 = new Web3(fm.getProvider());
+    let textBuffer = new Buffer.from(req.body.textToUpload)
+    let refHashes = []
+
+    //encrypt buffers here using nucypher
+
+    sendHash = async (hash, recipient, sender) => {
+        const provider = ethers.getDefaultProvider('rinkeby');
+        await provider.ready
+
+
+        let wallet = new ethers.Wallet(privateKey, provider);
+
+        const contract = new ethers.Contract(CONTRACT_ADDRESS,CONTRACT_ABI,provider).connect(wallet)
+
+        contract.sendMessage(recipient, hash/*+'|||'+sender*/).then((err, _res) => {
+            if(err){
+                console.log(err)
+            }
+            res.send(_res)
+        })
+    }
+
+    var didAddFile = new Promise(function(resolve, reject) {
+        ipfs.add(textBuffer, (err, response) => {
+            if (err) {
+                console.log(err)
+            }
+            resolve(response[0].hash)
+        })
+    })
+
+    didAddFile.then(hash => {
+        sendHash(hash,req.body.recipient,req.body.UID)
+    })
+
+}).get('/fortmatic-style', (req,res) => {
+    res.sendFile(__dirname+'/style.css')
+}).get('/fortmatic-script', (req,res) => {
+    res.sendFile(__dirname+'/fortmatic.js')
+}).get('/ethereum', (req,res) => {
+    res.sendFile(__dirname+'/ethereum.png')
+}).get('/json', (req, res) => {
+    res.send({
+        data:jsondata
+    })
+}).post('/loadMessages', (req,res) => {
+    beginListener = async () => {
+        const provider = ethers.getDefaultProvider('rinkeby');
+
+        await provider.ready
+        const contract = new ethers.Contract(CONTRACT_ADDRESS,CONTRACT_ABI,provider);
+        provider.resetEventsBlock(0)
+
+        contract.on('Message', (to_addr, invalid1, invalid2, text, receipt) => {
+            console.log(to_addr +" : "+ req.body.addr)
+            if(to_addr.toString() == req.body.addr.toString()){
+                var obj = {}
+
+                if(!isIPFS.multihash(text)){
+                    var a = text.split('|||')
+                    obj = {
+                        sender : "",
+                        recipient : to_addr,
+                        text : a[0]
+                    }
+                    if(!jsondata.includes(obj)){
+                        jsondata.push(obj)
+                    }
+                } else {
+                    ipfs.get(text, function (err, files) {
+                        files.forEach((file) => {
+                            obj = {
+                                sender : "",
+                                recipient : to_addr,
+                                text : file.content.toString('utf8')
+                            }
+                            if(!jsondata.includes(obj)){
+                                jsondata.push(obj)
+                            }
+                        })
+                    })
+                }
+            }
+        })
+
+        return 1;
+    }
+    beginListener().then(()=>{
+        res.redirect('/json')
+    })
+})
